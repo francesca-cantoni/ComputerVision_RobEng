f796c75ec45eeeda53dacb0f1c1f57f7f98efb78
Francesca <francescacantoni95@gmail.com>
2020-06-17 18:43:50 +0200

    Upload LAB7

---
+++ LAB_7/M-Files/path_config.m
@@ -0,0 +1,6 @@
+%% Configuring vlfeat library and paths
+addpath('libraries/vlfeat/toolbox/mex/mexw64');
+run('libraries/vlfeat/toolbox/vl_setup');
+addpath('libraries/vlfeat');
+addpath('libraries/mAP');
+addpath('libraries/show');
